61c6019466391c576165dc3baebe5b0948fbd60b
mati28 <niamymathias14@gmail.com>
2020-06-09 03:31:18 +0000

    orders status

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "pending and completed orders"
+ 
+version := "1.0.0"
+ 
+scalaVersion := "2.11.12"
+ 
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.4" % "provided"
+
+libraryDependencies += "org.apache.logging.log4j" % "log4j-core" % "2.11.1"
---
+++ src/main/scala/CompletedOrders.scala
@@ -0,0 +1,35 @@
+package net.atos.spark
+
+import org.apache.spark.SparkContext._
+
+import org.apache.spark.SparkContext
+import org.apache.log4j._
+
+object CompletedOrders {
+
+	def orderParser(line:String) ={
+		val orderItems = line.split(',')
+		(orderItems(1),orderItems(2),orderItems(3))
+	}
+
+	def main(args:Array[String]):Unit = {
+
+		Logger.getLogger("org").setLevel(Level.ERROR)
+		
+		val input = args(0)
+		val output = args(1)
+
+		val sc = new SparkContext()
+
+		val orders = sc.textFile(input)
+
+		val orderDateStatus = orders.map(orderParser)
+
+		val ordersFiltered = orderDateStatus.
+		filter(order=> (order._3 == "COMPLETE") || (order._3 == "PROCESSING"))
+
+		ordersFiltered.map(order => order._1+", "+order._2+", "+order._3).coalesce(1).saveAsTextFile(output)
+	}
+}
+
+
